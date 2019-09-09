be9c030a872c425c1536be9bb8b773b38556c88a
linh5942 <linh594@newschool.edu>
2019-09-09 12:12:46 +0800

    test2 update

--- 二資料處理-字串/src/test.java
+++ 二資料處理-字串/src/test.java
@@ -5,38 +5,39 @@ import java.util.Arrays;
 
 public class test {
 	public static void main(String[] args) {
-		String text= "人易科技:上 機 測 驗 - 演算法";	
-//		char result = text.charAt(4);
-//		int result2 = (int)result+65248;
-//		System.out.println(result);
-//		System.out.println((char)result2);
-//		System.out.println((int)result);
+		// TODO Auto-generated method stub
+
+		String text = "人易科技:上 機 測 驗 - 演算法";
+		System.out.println(text);
 		
-		String replaced = text.replaceFirst(":", "：");
+		String replaced = text.replaceFirst(":", "："); // ":" + 65248  =  "："
+		
 		System.out.println(replaced);
 		
-//		char result3 = text.charAt(13);
-//		System.out.println((int)result3);
-		
-		String trim = replaced;		
-		for(int i=0; i<trim.length(); i++) {
-			for(int j=i+2; j<trim.length(); j++) {
-				char result4 = trim.charAt(i);
-				char result5 = trim.charAt(j);
-//				System.out.println((int)result4);
-//				System.out.println((int)result5);
+		String trim = replaced;
+		int length = trim.length();
+	//	System.out.println(length);
+		for(int i=0;i<trim.length();i++)
+		{
+			//System.out.println((int)text.charAt(i));
+				int preIndex = i -1;
+				int nextIndex = i+1;
+				if(preIndex<0)
+					preIndex=0;
+				if(nextIndex > length)
+					nextIndex =length;
 				
-				if( ((int)result4>10000)&&((int)result5>10000) ) {
-					trim = trim.replaceFirst(" ", "");
-				}
-			}
+				char PreValue = trim.charAt(preIndex);
+				char NextValue = trim.charAt(nextIndex);
+					if( ( (int)PreValue - (int)trim.charAt(i) >10000) && ( (int)NextValue - (int)trim.charAt(i)>10000))
+					{
+						trim =trim.replaceFirst(" ", "");
+						length = trim.length()-1;
+					//	System.out.println(trim);
+					}
+						
 		}
-		
-//		String remove = text.replaceFirst(" ", "");
 		System.out.println(trim);
-		
-		String printString = trim.substring(5,9);
-		System.out.println(printString);
 		
 	}
 
